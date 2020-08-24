e4fdfbb59e9a389f771daaf6bb7ea78256652d3e
konstantinosTabakis <69812176+konstantinosTabakis@users.noreply.github.com>
2020-08-24 21:55:02 +0300

    game1

--- main.js
+++ main.js
@@ -38,6 +38,34 @@ document.addEventListener('DOMContentLoaded', () => {
       "name":"love",
       "img": "https://images.unsplash.com/photo-1504196877113-b6ec66380c41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80"
   },
+  {
+      "name":"dr1",
+      "img": "https://scontent.fath3-3.fna.fbcdn.net/v/t1.0-9/48417337_2189046587813722_8680532803940515840_o.jpg?_nc_cat=105&_nc_sid=8bfeb9&_nc_ohc=LeaMhcvdQM8AX-AB2jo&_nc_ht=scontent.fath3-3.fna&oh=033a382ab9778421f9c6c80834a2e697&oe=5F691ED4 "
+  },
+  {
+      "name":"dr2",
+      "img": " https://scontent.fath3-4.fna.fbcdn.net/v/t1.0-9/47455472_10210654790079630_1602872702421434368_o.jpg?_nc_cat=101&_nc_sid=e3f864&_nc_ohc=LMLK6hPN7oMAX-NPi0Q&_nc_ht=scontent.fath3-4.fna&oh=f74296b9213870d47359101e553f9a5e&oe=5F69A996"
+  },
+  {
+      "name":"dr3",
+      "img": "https://scontent.fath3-3.fna.fbcdn.net/v/t1.0-9/11902424_998320323553027_2621536523660519571_n.jpg?_nc_cat=111&_nc_sid=19026a&_nc_ohc=JDGbOI6WvUYAX-uhxxa&_nc_ht=scontent.fath3-3.fna&oh=a5d234320bf65cbe6104a7cab76c60ab&oe=5F688FF7 "
+  },
+  {
+      "name":"dr4",
+      "img": "dr4.png"
+  },
+  {
+      "name":"dr5",
+      "img": "dr5.png"
+  },
+  {
+      "name":"dr6",
+      "img": "dr6.png"
+  },
+  {
+      "name":"dr7",
+      "img": "dr7.png"
+  },
    
   {
       "name":"theater",
@@ -54,7 +82,7 @@ let cardArray= []
    cardsChosen = []
    cardsChosenId = []
    cardsWon = [] 
-   while(cardArray.length<=8){
+   while(cardArray.length<=10){
       let x=Math.floor(Math.random()*data.length)
      
      
@@ -79,15 +107,15 @@ let cardArray= []
 
 
   let messages = document.querySelector(".messages")
-  const easyBtn= document.querySelector(".easy")
-  const mediumBtn= document.querySelector(".medium")
-  const hardBtn= document.querySelector(".hard")
+  const easyBtn= document.querySelector("#easy")
+  const mediumBtn= document.querySelector("#medium")
+  const hardBtn= document.querySelector("#hard")
   easyBtn.addEventListener('click',function(){
-    tries_index=5
+    tries_index=8
     let cards = document.querySelectorAll("img")
 
     if (images){
-      for (let i = 0; i < 10;  i++) {
+      for (let i = 0; i < 12;  i++) {
         // cards[i].setAttribute("src", "white.png");
         cards[i].remove()
         }} 
@@ -96,10 +124,10 @@ let cardArray= []
   })
    
   mediumBtn.addEventListener('click',function(){
-    tries_index=4
+    tries_index=6
     let cards = document.querySelectorAll("img")
     if (images){
-      for (let i = 0; i < 10;  i++) {
+      for (let i = 0; i < 12;  i++) {
         // cards[i].setAttribute("src", "white.png");
         cards[i].remove()
         }} 
@@ -107,10 +135,10 @@ let cardArray= []
   })
    
   hardBtn.addEventListener('click',function(){
-    tries_index=3
+    tries_index=5
     let cards = document.querySelectorAll("img")
     if (images){
-      for (let i = 0; i < 10;  i++) {
+      for (let i = 0; i < 12;  i++) {
         // cards[i].setAttribute("src", "white.png");
         cards[i].remove()
         }} 
@@ -150,7 +178,7 @@ let cardArray= []
     if(optionOneId == optionTwoId) {
       cards[optionOneId].setAttribute('src', "back.jpg")
       cards[optionTwoId].setAttribute('src', "back.jpg")
-      alert('You have clicked the same image!')
+       messages.textContent=('You have clicked the same image!')
       tries_index-=1
       tries.textContent=tries_index
       if (tries_index==0){
@@ -192,19 +220,19 @@ let cardArray= []
     cardsChosenId.push(cardId)
     this.setAttribute('src', cardArray[cardId].img)
     if (cardsChosen.length ===2) {
-      setTimeout(checkForMatch, 200)
+      setTimeout(checkForMatch, 300)
     }
   }
 // 6 losing condition
   function lost() {
     let cards = document.querySelectorAll("img")
-    for (let i = 0; i < 10;  i++) {
+    for (let i = 0; i < 12;  i++) {
       // cards[i].setAttribute("src", "white.png");
       cards[i].remove()
       }
     images=false  
      
-    messages.textContent="Im sorry,you lost"
+    messages.textContent="Im sorry, you lost"
   }
  
 })
